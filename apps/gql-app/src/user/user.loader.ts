87235671cd983cfafbacbfcb536e3efb2afe1bfd
Mateusz Żupa <yetiasg@gmail.com>
2022-10-24 21:39:10 +0200

    Create users loader

--- apps/gql-app/src/user/user.loader.ts
+++ apps/gql-app/src/user/user.loader.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import * as DataLoader from 'dataloader';
 
-@Injectable()
-export class UserLoader {}
+@Injectable({ scope: Scope.REQUEST })
+export class UserLoader {
+  constructor(private readonly userService: UserService) {}
+
+  public readonly batchAuthors = new DataLoader(
+    async (authorsIds: string[]) => {
+      const users = await this.userService.getByIds(authorsIds);
+      const usersMap = new Map(users.map((user) => [user.id, user]));
+      return authorsIds.map((authorId) => usersMap.get(authorId));
+    },
+  );
+}
